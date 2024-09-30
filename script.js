3eaba1292851cda9f90feafe7bb99e794b38ce52
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-09-30 20:13:41 +0200

    Pridavam nove soubory

---
+++ script.js
@@ -0,0 +1,6 @@
+//Jeden lístek do divadla Pěst na oko stojí 12 euro. Spočítejte měsíční příjem divadla ze vstupného, přichází-li průměrně 174 návštěvníků na jedno představení a divadlo hraje 15 představení měsíčně. Uložte výsledek do proměnné prijem. Hodnotu proměnné prijem vypište do stránky. Divadlo se rozhodlo prodávat studentské vstupné ve výši 65 % plného vstupného. Jak se změní měsíční příjem divadla pokud víme, že 40 % návštěvníků jsou studenti? Vypište do stránky i příjem divadla se započítanou slevou pro studenty.
+
+let cena = 12
+let pocetNavstevniku = 174
+let pocetPredstaveni = 15
+
